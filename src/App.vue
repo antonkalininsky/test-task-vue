65efd1b584ee5bc38058ca92f2cc8c8bdc32e4ef
Anton <chadik.free@gmail.com>
2023-03-05 14:49:53 +0600

    * Подключение файлов - информация о товарах * Создание компонента - товар (ItemBox) * Создание компонента - основное окно пользователя (MainWindow) * Логика работы основного окна: выбор элемента для отображения (правая колонка), добавление и удаление в списке товаров (левая колонка)

--- src/App.vue
+++ src/App.vue
@@ -1,122 +1,15 @@
 <template>
-    <div class="main">
-        <div class="top-panel">
-            <div class="items-group-wrap">
-                <div class="items-group draw-box">
-                    <div class="item-selected draw-box item">item</div>
-                    <div class="item-selected draw-box item">item</div>
-                    <div class="item-selected draw-box item">item</div>
-                    <div class="item-selected draw-box item">item</div>
-                    <div class="item-selected draw-box item">item</div>
-                    <div class="item-selected draw-box item">item</div>
-                </div>
-                <div class="items-group-text">selected: n / m</div>
-            </div>
-            <div class="item-show draw-box">item-show</div>
-        </div>
-        <div class="bot-panel">
-            <div class="left-column draw-box">
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-            </div>
-            <div class="right-column draw-box">
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-                <div class="item-selected draw-box item">item</div>
-            </div>
-        </div>
-    </div>
+    <MainWindow />
 </template>
 
-<script setup></script>
+<script setup>
+import MainWindow from './components/MainWindow.vue';
+</script>
 
 <style>
 body,
 * {
     margin: 0;
     padding: 0;
-}
-</style>
-
-<style scoped>
-.main {
-    width: calc(100vw - 10px);
-    height: 90vh;
-    padding: 5px;
-}
-
-/* TOP PANEL */
-
-.top-panel {
-    margin: 0 auto 20px auto;
-
-    width: 100%;
-    height: 200px;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-}
-
-.draw-box {
-    border: 2px solid black;
-}
-
-.item-show {
-    width: 30%;
-}
-
-.items-group-wrap {
-    width: 30%;
-    position: relative;
-}
-
-.items-group {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-
-    padding: 10px;
-    height: 100%;
-    box-sizing: border-box;
-}
-
-.item {
-    width: 50px;
-    height: 50px;
-    margin: 5px;
-}
-
-.items-group-text {
-    position: absolute;
-    bottom: 5px;
-
-    width: 100%;
-    text-align: center;
-}
-
-/* BOT PANEL */
-.bot-panel {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-}
-
-.left-column,
-.right-column {
-    width: 49%;
-    height: 60vh;
-
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    
 }
 </style>
---
+++ src/components/ItemBox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="item-box">
+        <div class="id">ID: {{ data.id }}</div>
+        <div class="name">{{ data.name }}</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "ItemBox",
+    props: {
+        data: {
+            Object,
+            default: {
+                id: -1,
+                name: 'noname'
+            }
+        }
+    }
+};
+</script>
+<style scoped>
+.item-box {
+    width: 60px;
+    height: 60px;
+    margin: 5px;
+
+    border: 2px solid black;
+    cursor: pointer;
+
+    user-select: none;
+}
+</style>
